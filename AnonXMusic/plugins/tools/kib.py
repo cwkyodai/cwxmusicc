a3a94e55df317987af4ce5440da3ba83a5a1ea1c
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-04-19 19:56:15 +0200

    Update kib.py

--- AnonXMusic/plugins/tools/kib.py
+++ AnonXMusic/plugins/tools/kib.py
@@ -3,7 +3,6 @@ import asyncio
 import time
 import random
 from pyrogram import Client, filters
-from strings import get_command
 from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
 from AnonX import app
